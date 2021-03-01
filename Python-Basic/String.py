641bbed3c8e9a9f1b7524f999f722dae7288cdae
ryudongjae <rdj10149@gmail.com>
2021-03-01 18:08:15 +0900

    Commit

---
+++ Python-Basic/String.py
@@ -0,0 +1,23 @@
+string1 = "브이넥 라이트 다운 베스트"
+string2 = "      25,990원     "
+
+print(string1)
+print(string2)
+
+
+print(string1[0])
+print(string1[1])
+print(string1[2])
+print(string1[-1])
+print(string1[-2])
+print(string1[-3])
+
+
+print(string1[0:3])
+print(string1[4:7])
+print(string1[-6:-4])
+print(string2[6:13])
+
+
+print(string1[11:])
+print(string1[:7])
