b77af8d4a1165eb6fafb30cce5b8128c9987ef65
ryudongjae <rdj10149@gmail.com>
2021-03-01 19:48:17 +0900

    Commit

--- Python-Basic/String2.py
+++ Python-Basic/String2.py
@@ -1,2 +1,17 @@
 string1 = "브이넥 라이트 다운 베스트"
 string2 = "      25,990원     "
+
+
+
+print(string1.replace("라이트","헤비"))
+print(string1)
+
+string1 = string1.replace("라이트", "헤비")
+print(string1)
+
+
+print(string2.strip())
+print(string2)
+
+string2 =string2.strip()
+print(string2)
