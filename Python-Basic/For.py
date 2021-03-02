d915554080bb15252795694cb4b28c01335a8541
ryudongjae <rdj10149@gmail.com>
2021-03-02 14:22:32 +0900

    Commit

---
+++ Python-Basic/For.py
@@ -0,0 +1,9 @@
+MAX = 10
+
+for i in range(1, MAX):
+    print(i)
+    print("반복문을 배워 봅시다.")
+
+for i in range(1,MAX,2):
+    print(i)
+    print("반복문을 배워 봅시다.")
---
+++ Python-Basic/For2.py
@@ -0,0 +1,10 @@
+players =["손흥민","기성용","이승우","박지성","차두리"]
+
+print("2019년 아시안컵 촐전명단: ")
+for i in range(len(players)):
+    print(players[i])
+
+
+print("2019년 아시안컵 출전명단: ")
+for p in players:
+    print(p)
