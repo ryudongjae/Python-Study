83b8764e6e3204fa44949f8cba5c132349e82ebf
ryudongjae <rdj10149@gmail.com>
2021-03-02 15:40:48 +0900

    Commit

--- Challenge-ex/challenge2.py
+++ Challenge-ex/challenge2.py
@@ -1,26 +1,26 @@
-# data = ["조회수 : 1,500","조회수 : 1,002","조회수 : 300","조회수 : 251","조회수 : 13,432","조회수 : 998"]
-#
-# sum = 0
-#
-# for d in data:
-#     print(d)
-#
-# for d in data:
-#     data1 = d.replace("조회수 : ","")
-#     print(data1)
-#
-# for d in data:
-#     d = d[5:]
-#     d = d.replace(",","")
-#     print(d)
-#
-# for d in data:
-#     d = d[5:]
-#     d = d.replace(",","")
-#     d = int(d)
-#     sum = sum +d
-#     print(sum)
-#
+data = ["조회수 : 1,500","조회수 : 1,002","조회수 : 300","조회수 : 251","조회수 : 13,432","조회수 : 998"]
+
+sum = 0
+
+for d in data:
+    print(d)
+
+for d in data:
+    data1 = d.replace("조회수 : ","")
+    print(data1)
+
+for d in data:
+    d = d[5:]
+    d = d.replace(",","")
+    print(d)
+
+for d in data:
+    d = d[5:]
+    d = d.replace(",","")
+    d = int(d)
+    sum = sum +d
+    print(sum)
+
 def sumOfList(llist):
   sum = 0
   for n in llist:
