09a06db4bf97156c685ffa437a68b1e299f3eceb
ryudongjae <rdj10149@gmail.com>
2021-03-03 16:19:11 +0900

    Commit

---
+++ Challenge-ex/challenge5.py
@@ -0,0 +1,17 @@
+
+for i in range (1,11):
+    print("*"*i)
+
+
+
+for i in range(1,11, 2):
+    print("*"*i)
+
+
+for i in range(1,11):
+    print("*"*(11-i))
+
+
+
+for i in range(10,0,-1):
+    print("*"*i)
