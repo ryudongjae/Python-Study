b6fd317db1592f6c982d29e11fecc8f518a81e0b
ryudongjae <rdj10149@gmail.com>
2021-03-05 22:06:49 +0900

    Commit

--- Python-Basic/For2.py
+++ Python-Basic/For2.py
@@ -8,3 +8,15 @@ for i in range(len(players)):
 print("2019년 아시안컵 출전명단: ")
 for p in players:
     print(p)
+
+
+person = ["조인성","정우성","강동원","황정민","곽도원"]
+
+
+print("영화제 명단 : ")
+for i in range(len(person)):
+    print(person [i])
+
+print("영화제 명단 : ")
+for p in person:
+    print(p);
