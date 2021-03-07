abfa03d7dd86659a17a82d2fe8c7e6b042342317
ryudongjae <rdj10149@gmail.com>
2021-03-07 15:45:03 +0900

    Commit

--- Python-Basic/For.py
+++ Python-Basic/For.py
@@ -7,3 +7,12 @@ for i in range(1, MAX):
 for i in range(1,MAX,2):
     print(i)
     print("반복문을 배워 봅시다.")
+
+
+for i in range(1, MAX):
+    print(i)
+    print("반복문을 배워 봅시다.")
+
+for i in range(1,MAX,2):
+    print(i)
+    print("반복문을 배워 봅시다.")
