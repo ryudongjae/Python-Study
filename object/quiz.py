99da9d18416e42af20ed3b5b4bbe581cf4826af3
ryudongjae <rdj10149@gmail.com>
2021-03-17 16:14:14 +0900

    Update

---
+++ object/quiz.py
@@ -0,0 +1,7 @@
+station1 = "사당"
+station2 = "신도림"
+station3 = "인천공항"
+
+print(station1+"행 열차가 들어오고 있습니다.")
+print(station2+"행 열차가 들어오고 있습니다.")
+print(station3+"행 열차가 들어오고 있습니다.")
---
+++ object/quiz2.py
@@ -0,0 +1,5 @@
+from random import *
+
+func= randint(4,28)
+
+print("오프라인 스터디 모임 날짜는 매월 ",func,"일로 선정되었습니다.")
