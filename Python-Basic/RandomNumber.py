f47556259653d417ee9b77aff1e2022fb2238548
ryudongjae <rdj10149@gmail.com>
2021-03-17 16:13:59 +0900

    Update

---
+++ Python-Basic/RandomNumber.py
@@ -0,0 +1,16 @@
+from random import *
+
+print(random())
+print(random())
+print(random())
+print(random())
+print(random())
+print(int(random() * 10))
+print(int(random() * 10))
+print(int(random() * 10))
+print(int(random() * 10))
+print(int(random() * 10)+1)
+print(int(random() * 10)+1)
+print(int(random() * 10)+1)
+print(int(random() * 10)+1)
+
