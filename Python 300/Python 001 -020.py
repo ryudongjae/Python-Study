0e35d7793353db2be76241342d5652911a6250f0
ryudongjae <rdj10149@gmail.com>
2021-03-21 22:17:58 +0900

    Commit

---
+++ Python 300/Python 001 -020.py
@@ -0,0 +1,10 @@
+# 001 print 기초
+# 화면에 Hello World 문자열을 출력하세요.
+print("helloworld")
+
+# 002 print 기초
+# 화면에 Mary's cosmetics을 출력하세요. (중간에 '가 있음에 주의하세요)
+print("Mary's cosmetics")
+
+#003 print 기초
+# 화면에 아래 문장을 출력하세요. (중간에 "가 있음에 주의하세요.)
